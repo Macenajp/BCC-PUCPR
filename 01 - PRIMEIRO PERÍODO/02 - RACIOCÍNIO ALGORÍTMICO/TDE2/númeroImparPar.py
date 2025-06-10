bb286fd9a10500856d65cc1837021feb1e0f1f9c
João Pedro Macena <joaomacenactba@gmail.com>
2025-06-10 16:22:19 -0300

    Delete 01 - PRIMEIRO PERÍODO/02 - RACIOCÍNIO ALGORÍTMICO/TDE2 directory

--- 01 - PRIMEIRO PERÍODO/02 - RACIOCÍNIO ALGORÍTMICO/TDE2/númeroImparPar.py
+++
@@ -1,7 +0,0 @@
-# Questão 1 da lista:
-numero = int(input("Digite um número: "))
-
-if numero % 2 == 0:
-    print("Esse número é par.")
-else:
-    print("Esse número é ímpar.")
--- 01 - PRIMEIRO PERÍODO/02 - RACIOCÍNIO ALGORÍTMICO/TDE2/raizNúmero.py
+++
@@ -1,8 +0,0 @@
-# Questão 3 da lista:
-numero = int(input("Digite um número inteiro: "))
-
-if numero < 0:
-    print("Valor inválido")
-else:
-    raiz = numero ** 0.5
-    print("A raiz quadrada de", numero, "é", round(raiz, 2))
