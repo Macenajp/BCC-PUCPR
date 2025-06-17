de8016882f5811ccfa3493f8c640cfa812179132
João Pedro Macena <joaomacenactba@gmail.com>
2025-06-17 19:11:40 -0300

    Update and rename a to valorMaiorMenor.py

---
+++ 01 - PRIMEIRO PERÍODO/02 - RACIOCÍNIO ALGORÍTMICO/REVISÃO/IF/valorMaiorMenor.py
@@ -0,0 +1,8 @@
+primeiro_valor = input("Escolha o valor: ")
+segundo_valor = input("Escolha o valor: ")
+
+if primeiro_valor >= segundo_valor:
+    print("O primeiro valor que é", primeiro_valor, "é maior que segundo valor, que é", segundo_valor)
+elif segundo_valor >= primeiro_valor:
+    print("O segundo valor que é", segundo_valor, "é maior que o primeiro valor, que é", primeiro_valor)
+
