ede6f672576731f8c69b76d9321a7d2bcf86ab3f
João Pedro Macena <joaomacenactba@gmail.com>
2025-06-25 22:32:02 -0300

    Update jogoTabVerdadeComentado.py

--- 01 - PRIMEIRO PERÍODO/04 - RESOLUÇÃO DE PROB./jogoTabVerdadeComentado.py
+++ 01 - PRIMEIRO PERÍODO/04 - RESOLUÇÃO DE PROB./jogoTabVerdadeComentado.py
@@ -19,6 +19,7 @@ def jogo():
         conectivos = ["and", "or", "not P", "not Q", "P -> Q", "P <-> Q"]
         operação = random.choice(conectivos)
 
+        # Monta a expressão e calcula o resultado correto com base na variável "operação".
         if operação == "and":
             resultado = P and Q
             expressãoLógica = f"{P} and {Q}"
