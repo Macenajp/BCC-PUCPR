3ca39226fe6b92d49f5339dcbf116b8f0f3c780d
João Pedro Macena <joaomacenactba@gmail.com>
2025-08-12 19:07:24 -0300

    Create tarefa1.py

---
+++ 01 - PRIMEIRO PERÍODO/02 - RACIOCÍNIO ALGORÍTMICO/REVISÃO/GERAL/tarefa1.py
@@ -0,0 +1,27 @@
+'''
+anoDeNascimento = float(input('Insira o seu ano de nascimento: '))
+validacao = input('Você já fez aniversário esse ano (s/n)? ')
+anoAtual = 2025
+if validacao == 'n':
+   resultado = (anoAtual - 1) - anoDeNascimento
+   print('Sua idade é:', resultado)
+else:
+   segundoResultado = anoAtual - anoDeNascimento
+   print('Sua idade é:', segundoResultado)
+'''
+
+
+
+
+'''
+nota1 = int(input('Informe a sua nota: '))
+nota2 = int(input('Informe a sua nota: '))
+
+media = (nota1 + nota2) / 2
+
+if media >= 7:
+    print('Você foi aprovado!')
+else:
+    print('Você foi reprovado!')
+
+'''
