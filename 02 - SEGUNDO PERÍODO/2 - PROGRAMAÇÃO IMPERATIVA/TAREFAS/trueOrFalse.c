4e14582bdbfedf83cf8a52e330c81221fb7774dc
João Pedro Macena <joaomacenactba@gmail.com>
2025-08-12 12:07:38 -0300

    Create trueOrFalse.c

---
+++ 02 - SEGUNDO PERÍODO/2 - PROGRAMAÇÃO IMPERATIVA/TAREFAS/trueOrFalse.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdboo.h>
+
+int main()
+{
+
+    bool Frio = true;
+    bool Calor = False;
+
+    printf("%d\n", Frio);
+    printf("%d\n", Calor);
+    
+    return 0;
+}
