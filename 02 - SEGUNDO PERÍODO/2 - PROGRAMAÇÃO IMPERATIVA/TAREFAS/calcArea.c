09c9c36ce32611a29db622b8e30298196c1ce2b9
João Pedro Macena <joaomacenactba@gmail.com>
2025-08-12 10:03:25 -0300

    Create calcArea.c

---
+++ 02 - SEGUNDO PERÍODO/2 - PROGRAMAÇÃO IMPERATIVA/TAREFAS/calcArea.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+
+    double PI = 3.14;
+    double raio = 10.0;
+    double area = PI * raio * raio;
+
+    printf("%f\n", area);
+
+    return 0;
+}
