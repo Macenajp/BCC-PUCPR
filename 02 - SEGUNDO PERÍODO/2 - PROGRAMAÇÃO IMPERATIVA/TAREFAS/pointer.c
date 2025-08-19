9cd4e786fd59b203d9245d64461e3fada8e8c79c
João Pedro Macena <joaomacenactba@gmail.com>
2025-08-19 10:17:08 -0300

    Update pointer.c

--- 02 - SEGUNDO PERÍODO/2 - PROGRAMAÇÃO IMPERATIVA/TAREFAS/pointer.c
+++ 02 - SEGUNDO PERÍODO/2 - PROGRAMAÇÃO IMPERATIVA/TAREFAS/pointer.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+    #include <stdio.h>
 
-int main()
-{
-    int a = 10;
-    printf("Tamanho de a: %p\n", &a);
-    printf("Tamanho de a: %zu\n", sizeof(a));
+    int main()
+    {
+        int a = 10;
+        printf("Endereço de a: %p\n", &a);
+        printf("Tamanho de a: %zu\n", sizeof(a));
 
-    int* p = &a;
-    printf("Valor de p: %p\n", p);
-    printf("Valor do apontado por p: %d\n", *p);
-    printf("Endereço de p: %p\n", &p);
-    printf("Tamanho de p: %zu\n", sizeof(p));
-}
+        int* p = &a;
+        printf("Valor de p: %p\n", p);
+        printf("Valor do apontado por p: %d\n", *p);
+        printf("Endereço de p: %p\n", &p);
+        printf("Tamanho de p: %zu\n", sizeof(p));
+    }
