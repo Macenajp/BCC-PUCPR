19e640ad96b78adae7c8b85c3b4b4f104674c7fb
João Pedro Macena <joaomacenactba@gmail.com>
2025-08-25 11:16:38 -0300

    Update phpBasic.php

--- 02 - SEGUNDO PERÍODO/3 - PROGRAMAÇÃO WEB/TAREFAS/PHP/phpBasic.php
+++ 02 - SEGUNDO PERÍODO/3 - PROGRAMAÇÃO WEB/TAREFAS/PHP/phpBasic.php
@@ -1,12 +1,22 @@
-<html>
-	<head>
-		<style>
-			p {
-			
-			}
-		</style>
-	</head>
-	<body>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Página</title>
+<style>
+	.botao{
+		padding: 10px, 15px;
+		margin: 5px;
+		border: 1px solid #333;
+		cursos: pointer;
+	}
+
+	.botao:hover{
+		background-color: blue;
+	}
+</style>
+</head>
+<body>
 
 
 
@@ -16,5 +26,5 @@
 
 	$nome="Teste";
 	echo "<h1>Seja bem vindo a página de $"
-	</body>
+</body>
 </html>
