e305135ca4488542ad66a5a70c964e5f7a1fb9ee
João Pedro Macena <joaomacenactba@gmail.com>
2025-10-28 18:34:09 -0300

    Add files via upload

---
+++ 02 - SEGUNDO PERÍODO/02 - PROGRAMAÇÃO  IMPERATIVA/TAREFA/pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    printf("Endereço de a: %p\n", &a);
+    printf("Tamanho de a: %zu\n", sizeof(a));
+
+    int* p = &a;
+    printf("Valor de p: %p\n", p);
+    printf("Valor do apontado por p: %d\n", *p);
+    printf("Endereço de p: %p\n", &p);
+    printf("Tamanho de p: %zu\n", sizeof(p));
+}
