039e96d7950fed6e77ecfd1484549b409603f230
João Pedro Macena <joaomacenactba@gmail.com>
2025-11-17 01:07:43 -0300

    Create conexaoBD.php

---
+++ 02 - SEGUNDO PERÍODO/03 - PROGRAMAÇÃO WEB/STORM MOTORS/bancoDeDados/conexaoBD.php
@@ -0,0 +1,9 @@
+<?php
+
+    $dbHost = 'localhost:3306';
+    $dbUsername = 'root';
+    $dbSenha = 'PUC@1234';
+    $dbName = 'stormmotors';
+    $conexao = new mysqli($dbHost, $dbUsername, $dbSenha, $dbName);
+
+?>
