4a9f0da6e7d872f35710de8948f7daba4a154759
João Pedro Macena <joaomacenactba@gmail.com>
2026-03-03 10:56:33 -0300

    Create exemploMatemático.java

---
+++ 03 - TERCEIRO PERÍODO/03 - PROGRAMAÇÃO ORIENTADA A OBJETOS/exemploMatemático.java
@@ -0,0 +1,15 @@
+// Expressão matemática com valores inteiros:
+
+int r = 2;
+int t = 5;
+
+int a = r * 7 - t;
+System.out.println(a);
+
+int b = r * (7 - t);
+System.out.println(b);
+
+int k = 10;
+
+k = k + (t / 2);
+System.out.printl(k);
