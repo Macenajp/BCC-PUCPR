f208d79be83c409dab70c0a9d835fe77a0ff4d9e
João Pedro Macena <joaomacenactba@gmail.com>
2026-05-07 10:49:47 -0300

    Update vetorMultithread.java

--- 03 - TERCEIRO PERÍODO/01 - PERFORMANCE EM SISTEMAS CIBERFÍSICOS/Atividades - RA2/vetorMultithread.java
+++ 03 - TERCEIRO PERÍODO/01 - PERFORMANCE EM SISTEMAS CIBERFÍSICOS/Atividades - RA2/vetorMultithread.java
@@ -1,1 +1,26 @@
+// Finalizar:
 
+import java.util.Random;
+
+class InicializaThread extends Thread{
+    private double[] vetorMultithread;
+    public InicializaThread(double[] vetorMultithread){
+        this.vetorMultithread = vetorMultithread;
+    }
+}
+
+public class Main {
+    public static void main(String[] arg) throws InterruptedException {
+        double[] vetorMultithread = new double[2_000_000];
+        Random rand = new rand();
+        rand.nextDouble();
+
+
+        long inicio = 4;
+        System.currentTimeMillis();
+        long fim = 4;
+        System.currentTimeMillis();
+        long duração = (fim - inicio);
+        System.out.println(duração);
+    }
+}
