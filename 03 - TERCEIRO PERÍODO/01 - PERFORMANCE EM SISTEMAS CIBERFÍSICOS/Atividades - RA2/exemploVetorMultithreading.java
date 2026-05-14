900b133e10762a322e6b89416655c3a0da2cf619
João Pedro Macena <joaomacenactba@gmail.com>
2026-05-14 08:37:09 -0300

    Create exemploVetorMultithreading.java

---
+++ 03 - TERCEIRO PERÍODO/01 - PERFORMANCE EM SISTEMAS CIBERFÍSICOS/Atividades - RA2/exemploVetorMultithreading.java
@@ -0,0 +1,18 @@
+// Terminar / Arrumar:
+class InicializaVetor extends Thread {
+    private Double[] vetor;
+    private int inicio;
+    private int fim;
+    public InicializaVetor (Double[] vetor, int inicio, int fim){
+        this.vetor = vetor;
+        this.fim = fim;
+        this.inicio = inicio;
+    }
+    public void run(){
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException{
+    Double
