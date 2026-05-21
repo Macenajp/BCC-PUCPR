53f380b690dc59097faeb3334674361d0b7e2647
João Pedro Macena <joaomacenactba@gmail.com>
2026-05-21 10:42:40 -0300

    Update T3 - condiçãoCorrida.java

--- 03 - TERCEIRO PERÍODO/01 - PERFORMANCE EM SISTEMAS CIBERFÍSICOS/Atividades - RA2/T3 - condiçãoCorrida.java
+++ 03 - TERCEIRO PERÍODO/01 - PERFORMANCE EM SISTEMAS CIBERFÍSICOS/Atividades - RA2/T3 - condiçãoCorrida.java
@@ -1,12 +1,11 @@
-// Falta finalizar:
-
+/*
 class Conta{
     print double saldo;
     public Conta(){this.saldo = 0.0f;}
     public void deposita(double valor){this.saldo += valor;}
     public void saque(double valor){this.saldo -= valor;}
     public double getSaldo(){return this.saldo;}
-    
+
 }
 
 class Deposita extends Thread{
@@ -14,11 +13,20 @@ class Deposita extends Thread{
     public Deposita(Conta conta){this.conta = conta;}
     public void run(){
         Random random = new Random();
+
         while(true){
             double valor = random.nextDouble();
             this.conta.deposita(valor);
             System.out.println("DEPOSITA VALOR: " + valor + "SALDO: "+ this.conta.getSaldo());
             try{Thread.sleep(101);} catch (InterruptedException e) {throw new RuntimeException(e);}
         }
-    
+
 }
+
+class Main{
+    public static void main(String[] args) throws InterruptedException{
+
+    }
+}
+*/
+
