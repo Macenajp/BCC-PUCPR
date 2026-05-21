32e88dfe6c9e2e316db61ce46a0eeb1c8774b700
João Pedro Macena <joaomacenactba@gmail.com>
2026-05-21 08:27:57 -0300

    Atualizar o multithread4.java

--- 03 - TERCEIRO PERÍODO/01 - PERFORMANCE EM SISTEMAS CIBERFÍSICOS/Atividades - RA2/multithread4.java
+++ 03 - TERCEIRO PERÍODO/01 - PERFORMANCE EM SISTEMAS CIBERFÍSICOS/Atividades - RA2/multithread4.java
@@ -1,4 +1,3 @@
-// Vetor Multithreading:
 import java.util.Random;
 
 class AtvThread extends Thread {
@@ -7,48 +6,59 @@ class AtvThread extends Thread {
     private final int inicio;
     private final int fim;
 
-    public AtvThread(double[] numeros, int i, int i1) {
+    public AtvThread(double[] numeros, int inicio, int fim) {
         this.numeros = numeros;
-        this.inicio = i;
-        this.fim = i1;
+        this.inicio = inicio;
+        this.fim = fim;
     }
 
+    @Override
     public void run() {
-        try {
-            Thread.sleep(10);
-            for (int i = this.inicio; i < this.fim; i ++) {
-                this.numeros[i] = random.nextDouble();
-            }
-        } catch (InterruptedException exception) {
-            throw new RuntimeException("Erro");
+        // Removido o Thread.sleep() para não prejudicar a performance
+        for (int i = this.inicio; i < this.fim; i++) {
+            this.numeros[i] = random.nextDouble();
         }
     }
 }
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        double[] numeros = new double[2000000];
-        int valores = 0;
-        AtvThread AtvThread1 = new AtvThread(numeros, 0, 500000);
-        AtvThread AtvThread2 = new AtvThread(numeros, 500000, 1000000);
-        AtvThread AtvThread3 = new AtvThread(numeros, 1000000, 1500000);
-        AtvThread AtvThread4 = new AtvThread(numeros, 1500000, 2000000);
-        AtvThread1.start();
-        AtvThread2.start();
-        AtvThread3.start();
-        AtvThread4.start();
-        AtvThread1.join();
-        AtvThread2.join();
-        AtvThread3.join();
-        AtvThread4.join();
+        // 1. Aloca um vetor double com 200 milhões de entradas
+        // Dica: Usar underscores (_) facilita a leitura de números grandes no Java
+        int tamanho = 200_000_000;
+        double[] numeros = new double[tamanho];
+        
+        // Dividindo 200 milhões entre 4 threads (50 milhões cada)
+        AtvThread t1 = new AtvThread(numeros, 0, 50_000_000);
+        AtvThread t2 = new AtvThread(numeros, 50_000_000, 100_000_000);
+        AtvThread t3 = new AtvThread(numeros, 100_000_000, 150_000_000);
+        AtvThread t4 = new AtvThread(numeros, 150_000_000, 200_000_000);
+        
+        // 2. Inicializa o vetor
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
 
-        for (int i = 0; i < numeros.length; i++){
-            Double numero = numeros[i];
+        // 3. Após inicialização exibe a mensagem (Exatamente como o enunciado pede)
+        System.out.println("Encerrou inicalizacao");
+
+        // 4. Computa os valores (Mantido na thread principal para simplificar, como no seu código)
+        int valores = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            // Usando tipo primitivo "double" ao invés do objeto "Double" para melhor performance
+            double numero = numeros[i];
             if (numero > 0.25 && numero < 0.75) {
-                valores += 1;
+                valores++;
             }
         }
-        System.out.println(valores);
-        System.out.println("Encerrou a inicialização");
+        
+        // Exibe a quantidade computada
+        System.out.println("Quantidade de valores encontrados: " + valores);
     }
 }
