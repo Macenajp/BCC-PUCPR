3dfcb83f0180fa88036a1546917b4b7e8c34880e
João Pedro Macena <joaomacenactba@gmail.com>
2026-05-21 08:20:05 -0300

    Atualizar o vetorMultithreadT1.java

--- 03 - TERCEIRO PERÍODO/01 - PERFORMANCE EM SISTEMAS CIBERFÍSICOS/Atividades - RA2/vetorMultithreadT1.java
+++ 03 - TERCEIRO PERÍODO/01 - PERFORMANCE EM SISTEMAS CIBERFÍSICOS/Atividades - RA2/vetorMultithreadT1.java
@@ -1,25 +1,61 @@
-// Finalizar:
-
 import java.util.Random;
 
-class InicializaThread extends Thread{
+class InicializaThread extends Thread {
     private double[] vetorMultithread;
-    public InicializaThread(double[] vetorMultithread){
+
+    public InicializaThread(double[] vetorMultithread) {
         this.vetorMultithread = vetorMultithread;
+    }
+
+    public void run() {
+        Random rand = new Random();
+        for (int i = 0; i < vetorMultithread.length; i++) {
+            vetorMultithread[i] = rand.nextDouble();
+        }
+    }
+}
+
+class ComputaThread extends Thread {
+    private double[] vetorMultithread;
+    private int quantidade;
+
+    public ComputaThread(double[] vetorMultithread) {
+        this.vetorMultithread = vetorMultithread;
+        this.quantidade = 0;
+    }
+
+    public void run() {
+        for (int i = 0; i < vetorMultithread.length; i++) {
+            if (vetorMultithread[i] > 0.25 && vetorMultithread[i] < 0.75) {
+                quantidade++;
+            }
+        }
+    }
+
+    public int getQuantidade() {
+        return quantidade;
     }
 }
 
 public class Main {
     public static void main(String[] arg) throws InterruptedException {
-        double[] vetorMultithread = new double[2_000_000];
-        Random rand = new rand();
-        rand.nextDouble();
+        double[] vetorMultithread = new double[200000000];
 
+        long inicio = System.currentTimeMillis();
 
-        long inicio = 4;
-        System.currentTimeMillis();
-        long fim = 4;
-        System.currentTimeMillis();
+        InicializaThread threadInicializacao = new InicializaThread(vetorMultithread);
+        threadInicializacao.start();
+        threadInicializacao.join();
+
+        System.out.println("Encerrou inicalizacao");
+
+        ComputaThread threadComputacao = new ComputaThread(vetorMultithread);
+        threadComputacao.start();
+        threadComputacao.join();
+
+        System.out.println(threadComputacao.getQuantidade());
+
+        long fim = System.currentTimeMillis();
         long duração = (fim - inicio);
         System.out.println(duração);
     }
