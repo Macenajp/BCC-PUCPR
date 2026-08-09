84647b1e20d6c8b958fdf3e4e76f014737609fb7
João Pedro Macena <joaomacenactba@gmail.com>
2026-08-09 18:42:41 -0300

    Refactor Contador class with parameterized constructor

---
+++ 04 - QUARTO PERÍODO/01 - RESOLUÇÃO DE PROB. ESTRUTURADOS EM COMP./REVISÃO - POO/ORIENTAÇÃO A OBJETOS/exercícioSimples01.java
@@ -0,0 +1,33 @@
+class Contador {
+    private int valor;                // Encapsulamento 
+
+    public Contador(int valor) {      // Construtor
+        this.valor = valor;
+    }
+
+    public void zerar() {
+        this.valor = 0;
+    }
+
+    public void incrementar() {
+        this.valor++;
+    }
+
+    public int getValor() {
+        return this.valor;
+    }
+}
+
+
+
+public class exercicioSimples01 {
+    public static void main(String[] args) {
+        Contador contador = new Contador(5);
+
+        contador.incrementar();
+        System.out.println("Valor depois de ser incrementado: " + contador.getValor());
+        
+        contador.zerar();
+        System.out.println("Valor depois de zerar: " + contador.getValor());
+    }
+}
